c9df6b33081ee6e05715025e306a1241be2ece92
Zaharahashmi <142885057+Zaharahashmi@users.noreply.github.com>
2023-12-28 11:28:27 +0530

    Add files via upload

---
+++ JavaProb1.java
@@ -0,0 +1,40 @@
+public class BankAccount {
+    private int accountId;
+    private String name;
+    private double balance;
+    public BankAccount(int accountId, String name, double balance) {
+        this.accountId = accountId;
+        this.name = name;
+        this.balance = balance;
+    }
+    public void deposit(double amount) {
+        balance += amount;
+    }
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+    }
+    public void printReceipt() {
+        System.out.println("Account id: " + accountId);
+        System.out.println("Name: " + name);
+        System.out.println("Account Balance: Rs." + balance);
+        System.out.println("---------------------------");
+    }
+    public static void main(String[] args) {
+        BankAccount accountA = new BankAccount(12344, "Account A", 5000);
+        BankAccount accountB = new BankAccount(12345, "Account B", 2500);
+        accountA.withdraw(1500);
+        accountB.deposit(1500);
+        System.out.println("Transfer from Account A to B completed.");
+        accountA.printReceipt();
+        accountB.printReceipt();
+        accountB.withdraw(3000);
+        accountA.deposit(3000);
+        System.out.println("Transfer from Account B to A completed.");
+        accountA.printReceipt();
+        accountB.printReceipt();
+    }
+}
